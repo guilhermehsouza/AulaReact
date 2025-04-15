01ec35887e0f39978121b2dc3db854c14e11b7d6
Guilherme Henrique de Souza <guihenry@gmail.com>
2025-04-15 14:57:14 -0300

    15/4

--- exercicioscomponentes/src/Componentes/Exemplo1.jsx
+++ exercicioscomponentes/src/Componentes/Exemplo1.jsx
@@ -4,9 +4,7 @@ export default function Exemplo1({numero1, numero2})
 
     return (
         <div>
-            <h3>Exemplo 1 - Calculadora</h3>
-
-            A soma dos números é {soma}
+            A soma {numero1} + {numero2} é igual a {soma}
         </div>
     );
 }
