ea0170e664ea7baa659491d40aac0127cbc3c106
Guilherme Henrique de Souza <guihenry@gmail.com>
2025-07-29 15:02:56 -0300

    29/07

--- estudocss/src/App.jsx
+++ estudocss/src/App.jsx
@@ -1,8 +1,53 @@
+import "./App.css";
+
 export default function App()
 {
+    const centralizado = {textAlign : "center"};
+
+    const destaque = {
+        textAlign : "center",
+        border : "2px solid magenta",
+        color : "pink",
+        padding : "10px"
+    }
+
     return (
         <div>
             <h1>Estudo CSS</h1>
+
+            <p>
+                O CSS (<i>Cacasding Style Sheets</i>) é uma linguagem utilizada para estilizar <b>página web</b>, e será utilizada dentro ou fora do HTML.
+            </p>
+
+            <p>
+                Na aula de hoje, vamos estudar e entender como aplicar estilos CSS em componentes do React.
+            </p>
+
+            <p>
+                Utilizando o CSS podemos definir:
+            </p>
+
+            <ul>
+                <li>Definir cor para o texto, para o fundo do elemento</li>
+                <li>Fontes e tamanhos de letras</li>
+                <li>Posicionamento dos elementos na página</li>
+                <li>Espaçamento, bordas, sombras, etc...</li>
+                <li>Efeitos visuais e animações.</li>
+            </ul>
+
+            <p>
+                O HTML diz o que deve aparecer na página, enquanto o CSS diz como deve aparecer.
+            </p>
+
+            <p className="centro">
+                Cidade de Mococa
+            </p>
+
+            <p style={centralizado}>
+                <img src="mococa.jpg" alt="" />
+            </p>
+
+            <h3 style={destaque}>Fim da aula</h3>
             
         </div>
     );
