90e5ddeaa82d2da1b7d6aecc8627c2dcf7bcae25
Guilherme Henrique de Souza <guihenry@gmail.com>
2025-08-05 12:20:37 -0300

    05/8

---
+++ estudorotas/src/Paginas/SaoPaulo.jsx
@@ -0,0 +1,20 @@
+export default function SaoPaulo()
+{
+    return(
+        <div>
+            <h1>Seja bem vindo ao Estado de São Paulo</h1>
+
+            <p>
+                O estado de São Paulo é o mais rico e populoso do Brasil, conhecido por sua economia diversificada e indústria avançada. Sua capital, também chamada São Paulo, é a maior cidade do país e um importante centro financeiro e cultural. O estado destaca-se pela agricultura, tecnologia e infraestrutura desenvolvida.
+            </p>
+
+            <p>
+                <img src=""  />
+            </p>
+
+            <p>
+                <a href="/">Voltar para Home</a>
+            </p>
+        </div>
+    )
+}
