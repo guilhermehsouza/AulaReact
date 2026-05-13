9e6fdfbb2d6a1a5e40ce73fae3008ba754912c9b
Guilherme Henrique de Souza <guilhermehsouza16@gmail.com>
2026-05-13 14:51:01 -0300

    135

--- aula02/src/App.jsx
+++ aula02/src/App.jsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import Exemplo1 from "./paginas/Exemplo1";
+import Exemplo2 from "./paginas/Exemplo2";
 
 export default function App()
 {
@@ -8,7 +10,16 @@ export default function App()
 
       <div className="card">
         <h3>Chamada para o Exemplo 1</h3>
+        <Exemplo1 numero={33} />
+        <Exemplo1 numero={43} />
+        <Exemplo1 numero={2} />
+      </div>
 
+      <div className="card">
+          <h3>Chamadas para o Exemplo 2</h3>
+          <Exemplo2 numero1={33} numero2={34} />
+          <Exemplo2 numero1={42} numero2={-345} />
+          <Exemplo2 numero1={4.5} numero2={6.8} />
       </div>
 
     </div>
