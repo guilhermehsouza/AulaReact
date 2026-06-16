3b1e3e8259c86d8b4ac5e095a8c798783047032b
Guilherme Henrique de Souza <guilhermehsouza16@gmail.com>
2026-06-16 11:02:24 -0300

    16/06

--- TurmaA/aula02/src/App.jsx
+++ TurmaA/aula02/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Exemplo1 from "./Exemplo1";
 import Exercicio1 from "./Exercicio1";
+import Exercicio7 from "./Exercicio7";
 
 export default function App() {
   return (
@@ -16,7 +17,13 @@ export default function App() {
           <Exercicio1 numero={12} />
           <Exercicio1 numero={67} />
           <Exercicio1 numero={-5} />
+      </div>
 
+      <div className="card">
+          <h3>Exercício 7</h3>
+          <Exercicio7 celsius={30} />
+          <Exercicio7 celsius={20} />
+          <Exercicio7 celsius={39.5} />
       </div>
 
     </div>
